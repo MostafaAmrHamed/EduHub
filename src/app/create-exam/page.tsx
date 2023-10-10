cedcb4d7b8bcf697d0bf11f42e3672e12f544fe0
MostafaAmrHamed <Mostafaamro5619@gmail.com>
2023-10-10 10:47:32 +0300

    added cursor-pointer

--- src/app/create-exam/page.tsx
+++ src/app/create-exam/page.tsx
@@ -4,12 +4,12 @@ const CreateExam = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex items-center gap-2 md:gap-4 bg-primary-2 p-2 md:p-5 rounded-md w-fit text-xs sm:text-base  md:text-xl text-primary-3">
-        <div className="flex gap-1 items-center">
+        <div className="flex gap-1 items-center cursor-pointer">
           <AiFillDatabase />
           <p>Exam Details</p>
         </div>
         <IoIosArrowForward />
-        <div className="flex gap-1 items-center">
+        <div className="flex gap-1 items-center cursor-pointer">
           <AiFillCheckCircle />
           <p>Start Exam</p>
         </div>
