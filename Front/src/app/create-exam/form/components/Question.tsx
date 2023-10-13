98a1fd94bd357fde71d67e4fafce3b1c70d32c87
MostafaAmrHamed <Mostafaamro5619@gmail.com>
2023-10-13 18:22:31 +0300

    feat: ADD Front and Back folders

---
+++ Front/src/app/create-exam/form/components/Question.tsx
@@ -0,0 +1,13 @@
+const Question = () => {
+  return (
+    <div className="bg-primary-2">
+      <div className="flex content-between px-2">
+        <h1>Q[ ]</h1>
+        <p>X</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Question;
--- src/app/create-exam/form/page.tsx
+++
@@ -1,5 +0,0 @@
-const Form = () => {
-  return <div>Exam Form</div>;
-};
-
-export default Form;
