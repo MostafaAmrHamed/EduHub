fbc93aafaf87be7be9c6a7fad63c140b7c759ad9
MostafaAmrHamed <Mostafaamro5619@gmail.com>
2023-10-14 08:40:52 +0300

    fixed: difficulty imags

--- Front/src/app/(routes)/create-exam/ExamDetails.tsx
+++ Front/src/app/(routes)/create-exam/ExamDetails.tsx
@@ -112,12 +112,14 @@ const ExamDetails = () => {
           <div
             className={`flex gap-1 items-center justify-center w-[220px] h-[40px] md:w-[150px] md:h-[50px] border-2 bg-primary-2 rounded border-primary-3 cursor-pointer transition-all duration-300 hover:border-primary-1 ${
               examDetails.difficulty === "easy" && " !border-primary-1"
-            } `}
+            }`}
             onClick={() => {
               setExamDetails({ ...examDetails, difficulty: "easy" });
             }}
           >
-            <Image src={easy} alt="difficulty-easy" className="w-[20px]" />
+            <div className="relative w-5 h-7">
+              <Image src="/images/Easy.png" fill alt="difficulty-easy" />
+            </div>
             <p>Easy</p>
           </div>
           <div
@@ -128,7 +130,9 @@ const ExamDetails = () => {
               setExamDetails({ ...examDetails, difficulty: "medium" });
             }}
           >
-            <Image src={medium} alt="difficulty-medium" className="w-[20px]" />
+            <div className="relative w-5 h-7">
+              <Image src="/images/Medium.png" fill alt="difficulty-medium" />
+            </div>
             <p>Medium</p>
           </div>
           <div
@@ -139,7 +143,9 @@ const ExamDetails = () => {
               setExamDetails({ ...examDetails, difficulty: "hard" });
             }}
           >
-            <Image src={hard} alt="difficulty-hard" className="w-[20px]" />
+            <div className="relative w-5 h-7">
+              <Image src="/images/Hard.png" fill alt="difficulty-hard" />
+            </div>
             <p>Hard</p>
           </div>
         </div>
