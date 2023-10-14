cebcb8ac4100a82ab0d627ed4487b6a688e8de02
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-14 07:48:13 +0300

    feat: ADD config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`${process.cwd()}/src/environments/.env.development.local`],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3001),
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
