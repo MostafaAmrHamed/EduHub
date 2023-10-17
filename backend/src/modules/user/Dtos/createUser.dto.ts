e08f89b750d370bdd9cd8642a6418687f3e09aa4
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-17 09:33:45 +0300

    feat: ADD create User api docs

--- backend/src/modules/user/Dtos/createUser.dto.ts
+++ backend/src/modules/user/Dtos/createUser.dto.ts
@@ -4,24 +4,43 @@ import {
   IsNotEmpty,
   IsStrongPassword,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'John Doe',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
-
+  @ApiProperty({
+    example: 'test@gmail.com',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: '!Aa123456789',
+    required: true,
+    description:
+      'Must be a strong password that consists of letters, numbers, and symbols',
+  })
   @IsStrongPassword()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   phoneNumber: string;
-
+  @ApiProperty({
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   class: string;
