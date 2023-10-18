d27f1246fe779b18922a97637b030746dde728a6
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-18 11:03:02 +0300

    feat: ADD mongoose module to class module

--- backend/src/modules/classes/classes.module.ts
+++ backend/src/modules/classes/classes.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { ClassesController } from './classes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Class, ClassSchema } from './schema/class.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Class.name, schema: ClassSchema }]),
+  ],
   controllers: [ClassesController],
   providers: [ClassesService],
 })
