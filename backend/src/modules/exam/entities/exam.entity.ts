213a8a851b36997c4a22fd93f56a5f9a7383adfb
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-20 16:30:42 +0300

    feat: CREATE initial exam schema

--- backend/src/modules/exam/entities/exam.entity.ts
+++ backend/src/modules/exam/entities/exam.entity.ts
@@ -1,1 +1,30 @@
-export class Exam {}
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Class } from 'src/modules/classes/schema/class.entity';
+
+export enum Difficulty {
+  EASY = 'EASY',
+  MEDIUM = 'MEDIUM',
+  HARD = 'HARD',
+}
+@Schema({ timestamps: true })
+export class Exam {
+  @Prop({ required: true })
+  title: string;
+  @Prop({ required: true })
+  duration: string;
+  @Prop({ required: true, enum: Difficulty })
+  difficulty: Difficulty;
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Class.name,
+  })
+  class: mongoose.Schema.Types.ObjectId;
+  @Prop({ required: true })
+  questions: string;
+}
+
+export type ExamDocument = HydratedDocument<Exam>;
+
+export const ExamSchema = SchemaFactory.createForClass(Exam);
