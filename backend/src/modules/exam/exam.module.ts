168354650b8dc6f855bdcb79e79eb9758882b29f
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-20 16:33:02 +0300

    feat: ADD mongoose module

--- backend/src/modules/exam/exam.module.ts
+++ backend/src/modules/exam/exam.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ExamService } from './exam.service';
 import { ExamController } from './exam.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Exam, ExamSchema } from './entities/exam.entity';
 
 @Module({
   controllers: [ExamController],
   providers: [ExamService],
+  imports: [
+    MongooseModule.forFeature([{ name: Exam.name, schema: ExamSchema }]),
+  ],
 })
 export class ExamModule {}
