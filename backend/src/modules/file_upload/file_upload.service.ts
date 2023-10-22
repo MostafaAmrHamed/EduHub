bb685ae86ef9e25e094d4c5d1bb4ae71eb38b1b1
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 14:04:56 +0300

    feat: ADD file to db when uploaded

--- backend/src/modules/file_upload/file_upload.service.ts
+++ backend/src/modules/file_upload/file_upload.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import { InjectModel } from '@nestjs/mongoose';
+import { v2 } from 'cloudinary';
 import { Readable } from 'stream';
+import { File } from './schema/file.schema';
+import { Model } from 'mongoose';
 @Injectable()
 export class FileUploadService {
-  constructor() {}
-  async uploadImage(
-    file: Express.Multer.File,
-  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+  constructor(
+    @InjectModel(File.name) private readonly fileService: Model<File>,
+  ) {}
+  async uploadImage(file: Express.Multer.File) {
+    const fileExists = await this.fileService.findOne({
+      name: file.originalname,
+    });
+    if (fileExists) {
+      return fileExists;
+    }
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream((error, result) => {
+      const upload = v2.uploader.upload_stream(async (error, result) => {
         if (error) return reject(error);
-        resolve(result);
+        const uploadedFile = await this.fileService.create({
+          name: file.originalname,
+          link: result.secure_url,
+        });
+        resolve(uploadedFile);
       });
       const readable = new Readable();
       readable.push(file.buffer, 'base64');
