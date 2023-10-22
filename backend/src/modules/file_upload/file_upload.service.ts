6aca161fc588867452e210436ca475fe5001e39c
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 13:41:07 +0300

    feat: ADD file upload service

--- backend/src/modules/file_upload/file_upload.service.ts
+++ backend/src/modules/file_upload/file_upload.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
-
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import { Readable } from 'stream';
 @Injectable()
-export class FileUploadService {}
+export class FileUploadService {
+  constructor() {}
+  async uploadImage(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+      const readable = new Readable();
+      readable.push(file.buffer, 'base64');
+      readable.push(null);
+      readable.pipe(upload);
+    });
+  }
+}
