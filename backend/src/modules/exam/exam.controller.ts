08b882a0cd60eace46b2547e17e74148a64227dc
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 09:06:15 +0300

    feat: ADD rba to post, delete, update routes

--- backend/src/modules/exam/exam.controller.ts
+++ backend/src/modules/exam/exam.controller.ts
@@ -12,12 +12,15 @@ import { ExamService } from './exam.service';
 import { CreateExamDto } from './dto/create-exam.dto';
 import { UpdateExamDto } from './dto/update-exam.dto';
 import { JwtGuard } from '../auth/guards/jwt.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { Role } from '../user/schema/user.schema';
+import { AuthGuard } from '../auth/guards/role.guard';
 
-@UseGuards(JwtGuard)
+@UseGuards(JwtGuard, AuthGuard)
 @Controller('exam')
 export class ExamController {
   constructor(private readonly examService: ExamService) {}
-
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() createExamDto: CreateExamDto) {
     return this.examService.create(createExamDto);
@@ -32,12 +35,12 @@ export class ExamController {
   findOne(@Param('id') id: string) {
     return this.examService.findOne(+id);
   }
-
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateExamDto: UpdateExamDto) {
     return this.examService.update(+id, updateExamDto);
   }
-
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.examService.remove(+id);
