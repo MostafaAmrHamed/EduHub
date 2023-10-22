0976cbbbe81e75457b0f493033e6b32950a9855f
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 13:41:50 +0300

    feat: ADD upload logic to controller

--- backend/src/modules/file_upload/file_upload.controller.ts
+++ backend/src/modules/file_upload/file_upload.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileUploadService } from './file_upload.service';
-
-@Controller('file-upload')
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+@Controller('upload')
 export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileUploadService.uploadImage(file);
+  }
 }
