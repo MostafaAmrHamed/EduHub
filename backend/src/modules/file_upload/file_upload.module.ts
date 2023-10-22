096209f9ca8c313cd0b331877577f214069add14
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 13:54:59 +0300

    feat: ADD file schema to mongoose in file module

--- backend/src/modules/file_upload/file_upload.module.ts
+++ backend/src/modules/file_upload/file_upload.module.ts
@@ -3,10 +3,15 @@ import { FileUploadService } from './file_upload.service';
 import { FileUploadController } from './file_upload.controller';
 import { HttpModule } from '@nestjs/axios';
 import { CloudinaryProvider } from './cloudinary/cloudinary.provider';
+import { MongooseModule } from '@nestjs/mongoose';
+import { File, FileSchema } from './schema/file.schema';
 
 @Module({
   controllers: [FileUploadController],
   providers: [FileUploadService, CloudinaryProvider],
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([{ name: File.name, schema: FileSchema }]),
+  ],
 })
 export class FileUploadModule {}
