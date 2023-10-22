fa507d492c95682c3a28925ea56571e9cc01fe3b
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 08:28:39 +0300

    feat: ADD create exam dto

--- backend/src/modules/exam/dto/create-exam.dto.ts
+++ backend/src/modules/exam/dto/create-exam.dto.ts
@@ -1,1 +1,28 @@
-export class CreateExamDto {}
+import {
+  IsMongoId,
+  IsString,
+  IsNotEmpty,
+  IsEnum,
+  IsPositive,
+  Min,
+} from 'class-validator';
+import { Difficulty } from '../entities/exam.entity';
+import mongoose from 'mongoose';
+export class CreateExamDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+  @IsNotEmpty()
+  @IsPositive()
+  @Min(10)
+  duration: number;
+  @IsNotEmpty()
+  @IsEnum(Difficulty)
+  difficulty: Difficulty;
+  @IsNotEmpty()
+  @IsMongoId()
+  class: mongoose.Schema.Types.ObjectId;
+  //TODO: change to a questions schema id after creating a questions module
+  @IsNotEmpty()
+  questions: string;
+}
