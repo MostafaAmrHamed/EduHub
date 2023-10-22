de6c13e8258e3971718d9f580f135a44962287ab
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 10:41:48 +0300

    feat: ADD Api documentation

--- backend/src/modules/exam/exam.controller.ts
+++ backend/src/modules/exam/exam.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   UseGuards,
+  HttpStatus,
 } from '@nestjs/common';
 import { ExamService } from './exam.service';
 import { CreateExamDto } from './dto/create-exam.dto';
@@ -15,31 +16,65 @@ import { JwtGuard } from '../auth/guards/jwt.guard';
 import { Roles } from '../auth/decorators/role.decorator';
 import { Role } from '../user/schema/user.schema';
 import { AuthGuard } from '../auth/guards/role.guard';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Exam')
+@ApiBearerAuth('access_token')
 @UseGuards(JwtGuard, AuthGuard)
 @Controller('exam')
 export class ExamController {
   constructor(private readonly examService: ExamService) {}
+
+  @ApiBody({ type: CreateExamDto })
+  @ApiResponse({ status: HttpStatus.CREATED, type: CreateExamDto })
+  @ApiOperation({
+    summary: 'Creates an exam. ADMIN ONLY.',
+  })
   @Roles(Role.ADMIN)
   @Post()
   create(@Body() createExamDto: CreateExamDto) {
     return this.examService.create(createExamDto);
   }
 
+  @ApiResponse({ status: HttpStatus.OK, type: CreateExamDto, isArray: true })
+  @ApiOperation({
+    summary: 'Retrieves all exams.',
+  })
   @Get()
   findAll() {
     return this.examService.findAll();
   }
 
+  @ApiResponse({ status: HttpStatus.OK, type: CreateExamDto })
+  @ApiOperation({
+    summary: 'Retrieves single exam.',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.examService.findOne(+id);
   }
+
+  @ApiBody({ type: CreateExamDto })
+  @ApiResponse({ status: HttpStatus.OK, type: CreateExamDto })
+  @ApiOperation({
+    summary: 'Updates an exam. ADMIN ONLY',
+  })
   @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateExamDto: UpdateExamDto) {
     return this.examService.update(+id, updateExamDto);
   }
+
+  @ApiResponse({ status: HttpStatus.OK, type: CreateExamDto })
+  @ApiOperation({
+    summary: 'Deletes an exam. ADMIN ONLY',
+  })
   @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
