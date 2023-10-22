9f3ad595f6378224fb0f278843b0607763f8c266
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 13:40:07 +0300

    feat: ADD cloudinary to file_upload module

--- backend/src/modules/file_upload/file_upload.module.ts
+++ backend/src/modules/file_upload/file_upload.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FileUploadService } from './file_upload.service';
 import { FileUploadController } from './file_upload.controller';
+import { HttpModule } from '@nestjs/axios';
+import { CloudinaryProvider } from './cloudinary/cloudinary.provider';
 
 @Module({
   controllers: [FileUploadController],
-  providers: [FileUploadService],
+  providers: [FileUploadService, CloudinaryProvider],
+  imports: [HttpModule],
 })
 export class FileUploadModule {}
