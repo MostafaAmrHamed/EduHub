fc6b8c1bebf65a3b30e588e4cfdf556d089b7cee
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 09:05:33 +0300

    feat: ADD auth module to exam module imports

--- backend/src/modules/exam/exam.module.ts
+++ backend/src/modules/exam/exam.module.ts
@@ -3,12 +3,14 @@ import { ExamService } from './exam.service';
 import { ExamController } from './exam.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Exam, ExamSchema } from './entities/exam.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [ExamController],
   providers: [ExamService],
   imports: [
     MongooseModule.forFeature([{ name: Exam.name, schema: ExamSchema }]),
+    AuthModule,
   ],
 })
 export class ExamModule {}
