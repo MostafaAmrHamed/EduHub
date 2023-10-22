518350688de5190ec3364d8181bea1486ba54545
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-22 08:25:07 +0300

    feat: ADD initial code to exam service

--- backend/src/modules/exam/exam.service.ts
+++ backend/src/modules/exam/exam.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateExamDto } from './dto/create-exam.dto';
 import { UpdateExamDto } from './dto/update-exam.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Exam } from './entities/exam.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ExamService {
+  constructor(@InjectModel(Exam.name) private readonly examRepo: Model<Exam>) {}
+
   create(createExamDto: CreateExamDto) {
-    return 'This action adds a new exam';
+    return this.examRepo.create(createExamDto);
   }
 
   findAll() {
-    return `This action returns all exam`;
+    return this.examRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} exam`;
+    return this.examRepo.findById(id);
   }
 
   update(id: number, updateExamDto: UpdateExamDto) {
-    return `This action updates a #${id} exam`;
+    return this.examRepo.findByIdAndUpdate(id, updateExamDto, { new: true });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} exam`;
+    return this.examRepo.findByIdAndRemove(id);
   }
 }
