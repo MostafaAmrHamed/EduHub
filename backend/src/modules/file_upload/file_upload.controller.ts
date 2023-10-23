2de732bfd82c2a7b21c67414c477e52fcf369589
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-23 11:49:04 +0300

    feat: ADD file type validation to upload route

--- backend/src/modules/file_upload/file_upload.controller.ts
+++ backend/src/modules/file_upload/file_upload.controller.ts
@@ -1,5 +1,6 @@
 import {
   Controller,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -7,12 +8,18 @@ import {
 import { FileUploadService } from './file_upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
+import { FileTypeValidator } from './pipes/file_type.pipe';
 @Controller('upload')
 export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({ validators: [new FileTypeValidator({})] }),
+    )
+    file: Express.Multer.File,
+  ) {
     return this.fileUploadService.uploadImage(file);
   }
 }
