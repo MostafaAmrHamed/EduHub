61c269cf1a51d15deb4db4e03dca48bc0357ea9b
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-23 13:22:50 +0300

    feat: ADD response interface for create file

--- backend/src/modules/file_upload/file_upload.controller.ts
+++ backend/src/modules/file_upload/file_upload.controller.ts
@@ -1,5 +1,6 @@
 import {
   Controller,
+  HttpStatus,
   ParseFilePipe,
   Post,
   UploadedFile,
@@ -10,11 +11,19 @@ import { FileUploadService } from './file_upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { FileTypeValidator } from './pipes/file_type.pipe';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '../auth/guards/role.guard';
 import { Roles } from '../auth/decorators/role.decorator';
 import { Role } from '../user/schema/user.schema';
 import { JwtGuard } from '../auth/guards/jwt.guard';
+import { CreateFileResponse } from './dto/create_file.response';
 @ApiTags('Upload')
 @ApiBearerAuth('access_token')
 @Roles(Role.ADMIN)
@@ -23,7 +32,14 @@ import { JwtGuard } from '../auth/guards/jwt.guard';
 export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
   @Post()
+  @ApiOperation({
+    summary: 'Uploads an image. ADMIN ONLY',
+  })
   @ApiConsumes('multipart/form-data')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreateFileResponse,
+  })
   @ApiBody({
     schema: {
       type: 'object',
