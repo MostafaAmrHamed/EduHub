1fa086c81befa0f32d11a041a16f0b3b64f5f83f
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-24 09:49:36 +0300

    feat: ADD validation to create class dto

--- backend/src/modules/question/dto/create-question.dto.ts
+++ backend/src/modules/question/dto/create-question.dto.ts
@@ -1,1 +1,30 @@
-export class CreateQuestionDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsArray,
+  IsBoolean,
+  ValidateNested,
+  MinLength,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+export class CreateQuestionDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  @MinLength(2)
+  @ValidateNested({ each: true })
+  @Type(() => IAnswer)
+  answers: IAnswer[];
+}
+
+class IAnswer {
+  @IsString()
+  @IsNotEmpty()
+  choice: string;
+  @IsBoolean()
+  @IsNotEmpty()
+  isCorrect: string;
+}
