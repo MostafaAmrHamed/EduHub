0fa7f84cd354811d8a9078866493ce5495c34415
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-24 09:19:20 +0300

    feat: ADD initial question schema

--- backend/src/modules/question/entities/question.entity.ts
+++ backend/src/modules/question/entities/question.entity.ts
@@ -1,1 +1,19 @@
-export class Question {}
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type Answer = {
+  choice: string;
+  isCorrect: boolean;
+};
+@Schema({ timestamps: true })
+export class Question {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  answers: Answer[];
+}
+
+export type QuestionDocument = HydratedDocument<Question>;
+
+export const QuestionSchema = SchemaFactory.createForClass(Question);
