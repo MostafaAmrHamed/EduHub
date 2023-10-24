12fb365dfc4ae2259e99c349af6721c864472e3c
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-10-24 09:20:33 +0300

    feat: ADD auth module and mongoose module

--- backend/src/modules/question/question.module.ts
+++ backend/src/modules/question/question.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { AuthModule } from '../auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Question, QuestionSchema } from './entities/question.entity';
 
 @Module({
   controllers: [QuestionController],
   providers: [QuestionService],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      { name: Question.name, schema: QuestionSchema },
+    ]),
+  ],
 })
 export class QuestionModule {}
