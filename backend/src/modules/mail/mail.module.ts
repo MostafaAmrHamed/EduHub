ab9918e6c1fd75926f49e374bde4d8642851bc07
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-12-16 12:05:40 +0200

    feat: BOOTSTRAP MailModule

---
+++ backend/src/modules/mail/mail.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailController } from './mail.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  controllers: [MailController],
+  providers: [MailService],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const email = configService.get('email');
+        const password = configService.get('password');
+        return {
+          transport: `smtps://${email}:${password}@smtp.gmail.com`,
+          defaults: {
+            from: `"EduHub Support Team" <${email}>`,
+            subject: 'Welcome to EduHub',
+          },
+          template: {},
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class MailModule {}
