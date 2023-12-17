a54fff93554313432303a00167f897bc28823804
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-12-17 12:02:11 +0200

    feat: UPDATE exam schema with questions schema

--- backend/src/modules/exam/entities/exam.entity.ts
+++ backend/src/modules/exam/entities/exam.entity.ts
@@ -1,29 +1,37 @@
 import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { Class } from 'src/modules/classes/schema/class.entity';
+import { Question } from 'src/modules/question/entities/question.entity';
 
 export enum Difficulty {
   EASY = 'EASY',
   MEDIUM = 'MEDIUM',
   HARD = 'HARD',
 }
+export type ExamQuestions = InstanceType<typeof Question> & {
+  _id: mongoose.Types.ObjectId;
+};
+
 @Schema({ timestamps: true })
 export class Exam {
   @Prop({ required: true })
   title: string;
+
   @Prop({ required: true, min: 10 })
   duration: number;
+
   @Prop({ required: true, enum: Difficulty })
   difficulty: Difficulty;
+
   @Prop({
     required: true,
     type: mongoose.Schema.Types.ObjectId,
     ref: Class.name,
   })
   class: mongoose.Schema.Types.ObjectId;
-  //TODO: change to a questions schema id after creating a questions module
-  @Prop({ type: [mongoose.Schema.Types.ObjectId], default: [] })
-  questions: mongoose.Schema.Types.ObjectId[];
+
+  @Prop({ default: [] })
+  questions: ExamQuestions[];
 }
 
 export type ExamDocument = HydratedDocument<Exam>;
