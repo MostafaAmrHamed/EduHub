9212b1f2c6030f20805de8154cd425848e330f5b
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-12-17 11:57:25 +0200

    feat: ADD create method to question service

--- backend/src/modules/question/question.service.ts
+++ backend/src/modules/question/question.service.ts
@@ -1,34 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
-import { UpdateQuestionDto } from './dto/update-question.dto';
-import { InjectModel } from '@nestjs/mongoose';
+// import { UpdateQuestionDto } from './dto/update-question.dto';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Question } from './entities/question.entity';
+// import { Model } from 'mongoose';
 import { Question } from './entities/question.entity';
 import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Injectable()
 export class QuestionService {
   constructor(
-    @InjectModel(Question.name) private readonly questionRepo: Model<Question>,
+    @InjectModel(Question.name) private questionModel: Model<Question>,
   ) {}
-  create(createQuestionDto: CreateQuestionDto) {
-    return this.questionRepo.create(createQuestionDto);
-  }
-
-  findAll() {
-    return this.questionRepo.find();
-  }
-
-  findOne(id: number) {
-    return this.questionRepo.findById(id);
-  }
-
-  update(id: number, updateQuestionDto: UpdateQuestionDto) {
-    return this.questionRepo.findByIdAndUpdate(id, updateQuestionDto, {
-      new: true,
-    });
-  }
-
-  remove(id: number) {
-    return this.questionRepo.findByIdAndRemove(id);
+  create(createQuestionData: CreateQuestionDto | UpdateQuestionDto) {
+    const question = new this.questionModel(createQuestionData);
+    return question;
   }
 }
