0cab36e686cceebc1aa4eef2b5555a4f41a1599b
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-12-17 11:58:47 +0200

    feat: ENHANCE question creation with dto validation

--- backend/src/modules/question/dto/create-question.dto.ts
+++ backend/src/modules/question/dto/create-question.dto.ts
@@ -1,17 +1,62 @@
-import { IsNotEmpty, IsString, IsArray, ArrayMinSize } from 'class-validator';
+import {
+  IsString,
+  IsArray,
+  ValidateNested,
+  IsOptional,
+  ArrayMinSize,
+  Validate,
+} from 'class-validator';
 import { Type } from 'class-transformer';
-export class CreateQuestionDto {
-  @IsString()
-  @IsNotEmpty()
-  title: string;
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import mongoose from 'mongoose';
 
-  @IsNotEmpty()
+@ValidatorConstraint({ name: 'nonEmptyString', async: false })
+export class NonEmptyStringValidator implements ValidatorConstraintInterface {
+  validate(value: string) {
+    if (!value) {
+      return false;
+    }
+    return value.trim().length > 0;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return args.constraints[0]
+      ? args.constraints[0]
+      : 'Value cannot be an empty string';
+  }
+}
+
+export class QuestionBodyDto {
+  @IsOptional()
+  @IsString()
+  text: null | string;
+
+  @IsOptional()
+  @IsString()
+  image: null | string;
+}
+
+export class CreateQuestionDto {
+  @IsOptional()
+  _id: mongoose.Types.ObjectId;
+
+  @ValidateNested()
+  @Type(() => QuestionBodyDto)
+  question: QuestionBodyDto;
+
   @IsArray()
-  @Type(() => String)
-  @ArrayMinSize(2)
+  @IsString({ each: true })
+  @ArrayMinSize(2, { message: 'At least 2 answers are required' })
+  @Validate(NonEmptyStringValidator, {
+    message: 'Answers cannot contain empty strings',
+    each: true,
+  })
   answers: string[];
 
   @IsString()
-  @IsNotEmpty()
   correctAnswer: string;
 }
